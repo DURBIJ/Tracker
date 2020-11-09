1a9cb583c48c1064f5daf560395a04fbc3f04d73
DURBIJ <durbijkewat@gmail.com>
2020-11-09 20:52:24 +0530

    Registration Form completed

--- app/src/main/java/com/durbijkewat/tracker/Registration.java
+++ app/src/main/java/com/durbijkewat/tracker/Registration.java
@@ -2,13 +2,32 @@ package com.durbijkewat.tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.jar.Manifest;
 
 public class Registration extends AppCompatActivity {
+
+
+    Button RegisterBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+
+        RegisterBtn = findViewById(R.id.Register);
+
+        RegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
     }
 }
